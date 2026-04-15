b5c125280d8f95948b520c487e69b2eabe8625c7
50512 <me@50512.dev>
2026-04-15 01:23:10 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ sudo mkdir -p /usr/local/share/portalfetch
 # Copy the quotes database
 sudo cp quotes.txt /usr/local/share/portalfetch/
 sudo cp -r logos/ /usr/local/share/portalfetch/logos/
+sudo cp -r lib/ /usr/local/share/portalfetch/lib/
+sudo cp -r config/ /usr/local/share/portalfetch/config/
+
+sudo chmod +x /usr/local/share/portalfetch/lib/get_ssh_client.sh
+sudo chmod +x /usr/local/share/portalfetch/lib/get_terminal.sh
 
 # Install the executable script globally
 sudo cp portalfetch /usr/local/bin/portalfetch
