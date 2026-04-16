2a2cf7f0ede41fa5b0557091292c092f0ab19675
50512 <me@50512.dev>
2026-04-15 22:25:04 -0500

    create uninstall.sh script

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Uninstalling portalfetch..."
+
+sudo rm -r /usr/local/share/portalfetch
+sudo rm /usr/local/bin/portalfetch
+
+echo "Portalfetch uninstalled. And when you're dead I will be still alive."
